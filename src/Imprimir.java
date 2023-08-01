8cd500c1b2ad709e66d02b7ac9b0dc78b0a7757c
Paul Hidalgo <paulesh5@gmail.com>
2023-08-01 09:48:11 -0500

    Actualizar registro

--- src/Imprimir.java
+++ src/Imprimir.java
@@ -13,6 +13,24 @@ public class Imprimir {
     private JButton eliminarButton;
     private JPanel rootPanel;
     private JTextField textoPanel;
+    private JButton registrarButton;
+    private JTextField idIngreso;
+    private JTextField nombreIngreso;
+    private JTextField edadIngreso;
+    private JTextField ciudadIngreso;
+    private JLabel id;
+    private JTextField cedulaIngreso;
+    private JLabel nombre;
+    private JLabel edad;
+    private JLabel ciudad;
+    private JLabel cedula;
+    private JLabel password;
+    private JTextField claveIngreso;
+    private JTextField idIngreso2;
+    private JTextField nombreIngreso2;
+    private JLabel id2;
+    private JLabel nombre2;
+    private JButton actualizarButton;
 
     public Imprimir() {
         try (
@@ -37,6 +55,49 @@ public class Imprimir {
                         Statement stmt = conn.createStatement();
                 ) {
                     stmt.executeUpdate(query);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        registrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(idIngreso.getText());
+                String nombre = nombreIngreso.getText();
+                int edad = Integer.parseInt(edadIngreso.getText());
+                String ciudad = ciudadIngreso.getText();
+                String cedula = cedulaIngreso.getText();
+                String clave = claveIngreso.getText();
+                String query = "INSERT INTO ESTUDIANTES VALUES (" + id + ", '" + nombre + "', " + edad + ", '" + ciudad + "', '" + cedula + "', '" + clave + "')";
+                try (
+                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                        Statement stmt = conn.createStatement();
+                ) {
+                    stmt.executeUpdate(query);
+                    System.out.println("Nuevo estudiante registrado correctamente.");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(idIngreso2.getText());
+                String nombre = nombreIngreso2.getText();
+                String query = "UPDATE ESTUDIANTES SET NOMBRE = '" + nombre + "' WHERE ID = '" + id + "'";
+                try (
+                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                        Statement stmt = conn.createStatement();
+                ) {
+                    int rowsAffected = stmt.executeUpdate(query);
+                    if (rowsAffected > 0) {
+                        System.out.println("Nombre del estudiante actualizado correctamente.");
+                    } else {
+                        System.out.println("No se encontró ningún estudiante con el ID especificado.");
+                    }
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
